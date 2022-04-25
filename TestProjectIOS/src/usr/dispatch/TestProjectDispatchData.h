00473f7174af00dd370bde7429796a1de4c1f378
liwenfan <liwenfan@yitong.com.cn>
2022-04-25 14:42:27 +0800

    feat:dispatch_data的使用

--- TestProjectIOS/src/usr/dispatch/TestProjectDispatchData.h
+++ TestProjectIOS/src/usr/dispatch/TestProjectDispatchData.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface TestProjectDispatchData : NSObject
 
+- (void)mapData;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- TestProjectIOS/src/usr/dispatch/TestProjectDispatchData.m
+++ TestProjectIOS/src/usr/dispatch/TestProjectDispatchData.m
@@ -7,6 +7,12 @@
 
 #import "TestProjectDispatchData.h"
 
+@interface TestProjectDispatchData ()
+
+@property (nonatomic, weak) dispatch_data_t dispatchData;
+
+@end
+
 @implementation TestProjectDispatchData
 
 - (instancetype)init {
@@ -19,21 +25,44 @@
 - (void)testRunCreateDispatchData {
     NSData *data = [@"123" dataUsingEncoding:NSUTF8StringEncoding];
     
-    dispatch_data_t dispatchData = dispatch_data_create([data bytes], [data length], dispatch_get_main_queue(), ^{
+    self.dispatchData = dispatch_data_create([data bytes], [data length], dispatch_get_main_queue(), ^{
         NSLog(@"创建成功");
         DISPATCH_DATA_DESTRUCTOR_FREE;
     });
-    
-    [NSTimer scheduledTimerWithTimeInterval:2 repeats:NO block:^(NSTimer * _Nonnull timer) {
-        size_t dataSize = dispatch_data_get_size(dispatchData);
-        NSLog(@"%ld", dataSize);
-    }];
-    
-   const void *buffer;
-    size_t size;
-    dispatch_data_t mapDispatchData = dispatch_data_create_map(dispatchData, &buffer, &size);
-    NSLog(@"%@--%ld", buffer, size);
 
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//        [NSTimer scheduledTimerWithTimeInterval:3 repeats:NO block:^(NSTimer * _Nonnull timer) {
+//            NSLog(@"进来了吗");
+//            size_t dataSize = dispatch_data_get_size(self.dispatchData);
+//            NSLog(@"%ld", dataSize);
+//
+//    //        const void *buffer1;
+//    //        size_t size1;
+//    //        dispatch_data_t mapDispatchData1 = dispatch_data_create_map(dispatchData, &buffer1, &size1);
+//    //        NSLog(@"初始的数据:%@-大小:%ld-内容:%s-大小:%ld", mapDispatchData1, size1, buffer1, dispatch_data_get_size(mapDispatchData1));
+//
+//            const void *buffer;
+//            size_t size;
+//            dispatch_data_t subDispatchData = dispatch_data_create_subrange(self.dispatchData, 6, 1);
+//            dispatch_data_t mapDispatchData = dispatch_data_create_map(subDispatchData, &buffer, &size);
+//            NSLog(@"截取的数据:%@-大小:%ld-内容:%s", subDispatchData, size, buffer);
+//
+//
+//        }];
+//
+//    });
+}
+
+- (void)mapData {
+    void *buffer;
+    size_t size;
+//    dispatch_data_t subDispatchData = dispatch_data_create_subrange(self.dispatchData, 0, 9);
+    dispatch_data_t mapDispatchData = dispatch_data_create_map(self.dispatchData, &buffer, &size);
+    NSLog(@"截取的数据:%@-大小:%ld-内容:%s", mapDispatchData, size, buffer);
+    
+    NSData *mapData = [NSData dataWithBytes:&buffer length:size];
+    NSString *mapStr = [[NSString alloc] initWithData:mapData encoding:NSUTF8StringEncoding];
+    NSLog(@"转换后的str: %@", mapStr);
 }
 
 @end
